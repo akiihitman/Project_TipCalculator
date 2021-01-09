e4c67c32ed774f79813ff46327af19cfcfa0507d
Akash Kumar Singh <akiihitman1999@gmail.com>
2021-01-09 16:41:27 +0530

    Initial commit

---
+++ Project_TipCalculator/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Project_TipCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTipperPerson = new System.Windows.Forms.Button();
+            this.btnTotalperPerson = new System.Windows.Forms.Button();
+            this.btnCalculateBoth = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTipperPerson
+            // 
+            this.btnTipperPerson.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnTipperPerson.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTipperPerson.Location = new System.Drawing.Point(392, 190);
+            this.btnTipperPerson.Name = "btnTipperPerson";
+            this.btnTipperPerson.Size = new System.Drawing.Size(203, 51);
+            this.btnTipperPerson.TabIndex = 0;
+            this.btnTipperPerson.Text = "Tip Per Person";
+            this.btnTipperPerson.UseVisualStyleBackColor = false;
+            this.btnTipperPerson.Click += new System.EventHandler(this.btnTipperPerson_Click);
+            // 
+            // btnTotalperPerson
+            // 
+            this.btnTotalperPerson.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnTotalperPerson.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTotalperPerson.Location = new System.Drawing.Point(392, 247);
+            this.btnTotalperPerson.Name = "btnTotalperPerson";
+            this.btnTotalperPerson.Size = new System.Drawing.Size(203, 55);
+            this.btnTotalperPerson.TabIndex = 1;
+            this.btnTotalperPerson.Text = "Total Per Person";
+            this.btnTotalperPerson.UseVisualStyleBackColor = false;
+            this.btnTotalperPerson.Click += new System.EventHandler(this.btnTotalperPerson_Click);
+            // 
+            // btnCalculateBoth
+            // 
+            this.btnCalculateBoth.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCalculateBoth.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculateBoth.Location = new System.Drawing.Point(392, 308);
+            this.btnCalculateBoth.Name = "btnCalculateBoth";
+            this.btnCalculateBoth.Size = new System.Drawing.Size(203, 60);
+            this.btnCalculateBoth.TabIndex = 2;
+            this.btnCalculateBoth.Text = "Calculate Both";
+            this.btnCalculateBoth.UseVisualStyleBackColor = false;
+            this.btnCalculateBoth.Click += new System.EventHandler(this.btnCalculateBoth_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(100, 251);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(133, 42);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(100, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Bill :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(158, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(158, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 28);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tip Percent :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(158, 153);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 28);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "No of People :";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(466, 45);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 28);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(301, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(159, 24);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Tip Per Person :";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(466, 135);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(100, 28);
+            this.textBox5.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(286, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(174, 24);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Total per Person :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 382);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculateBoth);
+            this.Controls.Add(this.btnTotalperPerson);
+            this.Controls.Add(this.btnTipperPerson);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTipperPerson;
+        private System.Windows.Forms.Button btnTotalperPerson;
+        private System.Windows.Forms.Button btnCalculateBoth;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Project_TipCalculator/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_TipCalculator
+{
+    public partial class Form1 : Form
+    {
+        double Bill, TipPercent, NoofPeople, TipperPerson, TotalperPerson, TotalTip; // variables to store some value
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)    // method to clear all the entered data in the textbox
+        {
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+            textBox3.Text = " ";
+            textBox4.Text = " ";
+            textBox5.Text = " ";
+        }
+
+        private void btnCalculateBoth_Click(object sender, EventArgs e)  // method to calculate both Tip per person and Total Per Person
+        {
+            if (textBox1.Text == "" || textBox2.Text == " " || textBox3.Text == "") //text box field cannot be empty 
+            {
+                MessageBox.Show("Please fill all Required Details ", ("TextBox Validation"), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                Bill = Convert.ToDouble(textBox1.Text);
+                TipPercent = Convert.ToDouble(textBox2.Text);
+                NoofPeople = Convert.ToDouble(textBox3.Text);
+                TotalTip = Bill * (TipPercent / 100);
+                TipperPerson = TotalTip / NoofPeople;
+                textBox4.Text = TipperPerson.ToString();
+
+                Bill = Convert.ToDouble(textBox1.Text);
+                TipPercent = Convert.ToDouble(textBox2.Text);
+                NoofPeople = Convert.ToDouble(textBox3.Text);
+                TotalperPerson = ((Bill + TotalTip) / NoofPeople);
+                textBox5.Text = TotalperPerson.ToString();
+            }
+        }
+
+        private void btnTotalperPerson_Click(object sender, EventArgs e)  // method to calculate total per person
+        {
+            if (textBox1.Text == "" || textBox2.Text == " " || textBox3.Text == "") //text box field cannot be empty 
+            {
+                MessageBox.Show("Please fill all Required Details ", ("TextBox Validation"), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                Bill = Convert.ToDouble(textBox1.Text);
+                TipPercent = Convert.ToDouble(textBox2.Text);
+                NoofPeople = Convert.ToDouble(textBox3.Text);
+                TotalperPerson = ((Bill + TotalTip) / NoofPeople);
+                textBox5.Text = TotalperPerson.ToString();
+            }
+        }
+
+        private void btnTipperPerson_Click(object sender, EventArgs e)  // method to calculate tip per person
+        {
+            if (textBox1.Text == "" || textBox2.Text == " " || textBox3.Text == "")  //text box field cannot be empty 
+            {
+                MessageBox.Show("Please fill all Required Details ", ("TextBox Validation"), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+
+                Bill = Convert.ToDouble(textBox1.Text);
+                TipPercent = Convert.ToDouble(textBox2.Text);
+                NoofPeople = Convert.ToDouble(textBox3.Text);
+                TotalTip = Bill * (TipPercent / 100);
+                TipperPerson = TotalTip / NoofPeople;
+                textBox4.Text = TipperPerson.ToString();
+            }
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)  // this method does not allow  user to  entered an alphabet , it always allow numbers to enter in textfield
+        {
+            if (char.IsDigit(e.KeyChar) == false && char.IsControl(e.KeyChar) == false && (e.KeyChar == '.' ? textBox1.Text.Contains('.') == true : true))
+                e.Handled = true;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) == false && char.IsControl(e.KeyChar) == false && (e.KeyChar == '.' ? textBox2.Text.Contains('.') == true : true))
+                e.Handled = true;
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) == false && char.IsControl(e.KeyChar) == false && (e.KeyChar == '.' ? textBox3.Text.Contains('.') == true : true))
+                e.Handled = true;
+        }
+
+    }
+}
+
+// Test Cases 
+/*
+     S.No.                Input                             Otput
+               Bill   TipPercent  No.ofPeople      TipperPerson  TotalPerPerson   
+ 
+      1.      500       10           2               25            275
+      2.      250       20           4              12.5           75
+      3.      1000      30           6              50             216.66
+      4.      200       40           7              11.42          40
+      5.      5000      12.5         3              208.33         1875
+      6.      10000      50          4              1250           3750
+      7.      100       100         50               2             4
+      8.       50        2           10              0.1           5.1
+      9.      700       10           5               14            154  
+      10.     150        5           3               2.5           52.5
+ 
+ */
+
